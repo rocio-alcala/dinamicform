3fc9742ed9e86488b5af80892392be2dd3a4703c
Rocio Alcala <rro.alcala@gmail.com>
2024-02-06 16:55:34 -0300

    implementing react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/organisms/QuoteForm.tsx";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import QuoteCriteria from "./components/views/QuoteCriteria.tsx";
+import Travelers from "./components/views/Travelers.tsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <QuoteCriteria /> },
+  { path: "/quote", element: <h1>Quotes from backend</h1> },
+  { path: "/travelers", element: <Travelers /> }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={setupStore()}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
